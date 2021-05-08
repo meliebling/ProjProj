dddf6cc5384a96f7f84ca45fe44bf16ed3f602bf
meliebling <mebelyakov@gmail.com>
2021-05-09 02:00:40 +0300

    goog

---
+++ src/test/java/com/telran/project/FirstSeleniumTest.java
@@ -0,0 +1,39 @@
+package com.telran.project;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Locale;
+
+public class FirstSeleniumTest {
+    WebDriver wd;
+
+    @BeforeMethod
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.get("https://www.google.com/");
+
+
+    }
+    @Test
+    public void openGoogleTest(){
+        System.out.println("Site opened");
+        wd.findElement(By.name("q")).click();
+        wd.findElement(By.name("q")).sendKeys("Selenium"+ Keys.ENTER);
+        String text = wd.findElement(By.tagName("h3")).getText();
+        Assert.assertEquals(text.toLowerCase(),"selenium");
+
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+wd.quit();
+    }
+}
